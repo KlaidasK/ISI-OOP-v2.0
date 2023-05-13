d5c25332f01a2cf1fa20c5b798c215abc58998aa
KlaidasK <116492352+KlaidasK@users.noreply.github.com>
2023-05-13 23:39:55 +0300

    Add files via upload

---
+++ Source/DLL.h
@@ -0,0 +1,309 @@
+﻿#pragma once
+#ifndef _DLL_H_
+#define _DLL_H_
+
+
+
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+#include <algorithm>
+#include <limits>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <random>
+#include <chrono>
+#include <iterator>
+#include <sstream>
+#include <windows.h>
+
+#ifdef DLL_EXPORTS
+#define DLL_API __declspec(dllexport)
+#else
+#define DLL_API __declspec(dllimport)
+#endif
+
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::string;
+using std::copy;
+using std::numeric_limits;
+using std::vector;
+using std::istringstream;
+using std::istringstream;
+using std::runtime_error;
+
+
+
+/// Klasė "zmogus" yra abstrakčioji bazinė klasė
+/// 
+/// Klasė "zmogus" turi viena pure virtual funkcija virtual ~zmogus()=0 , dėl to negalima tiesiogiai kurti šios klasės objektų. Ji naudojama su kita derived klase "studentas".
+class zmogus {
+protected:
+    /// String tipo kintamasis vardas saugoja žmogaus vardą.
+    string vardas;
+    /// String tipo kintamasis pavarde saugoja žmogaus pavardę.
+    string pavarde;
+    /// Klasės konstruktorius.
+    /// <param name="var"></param> vardo parametras.
+    /// <param name="pav"></param> pavardės parametras.
+    zmogus(string var="", string pav="") {
+        vardas = var;
+        pavarde = pav;
+    }
+
+    /// Pure virtual destruktorius.
+    /// 
+    /// Pure virtual destruktorius ištrina string vardo ir pavardės reikšmes ir vieta, taip pat užtikrina, kad klasė bus abstrakčioji.
+    /// <returns></returns> 0.
+    DLL_API virtual ~zmogus() = 0;
+};
+
+/// Derived klasė "studentas"
+/// 
+/// Derived klasė iš class<zmogus>. Pasiima kintamuosius vardas ir pavarde.
+class studentas : public zmogus
+{
+private:
+    vector<int> paz;
+    int egz;
+    float vid, med;
+
+public:
+    /// Default konstruktorius
+    studentas() : zmogus() {
+        this->paz = {};
+        this->egz = 0;
+        this->vid = 0;
+        this->med = 0;
+    }
+    /// Konstruktorius
+    /// <param name="v"></param> vardo parametras.
+    /// <param name="p"></param> pavardės parametras.
+    /// <param name="g"></param> pažymių parametras.
+    /// <param name="e"></param> egzamino parametras.
+    studentas(string v, string p, vector<int> g, int e) : zmogus(v, p)
+    {
+        this->paz = g;
+        this->egz = e;
+        vid = vidurk();
+        med = median();
+    }
+
+    /// Kopijavimo konstruktorius
+    /// <param name="other"></param>
+    studentas(const studentas& other) : zmogus(other.vardas, other.pavarde)
+    {
+        this->paz = other.paz;
+        this->egz = other.egz;
+        this->vid = other.vid;
+        this->med = other.med;
+    }
+
+    /// Kopijavimo paskyrimas
+    studentas& operator=(const studentas& rhs)
+    {
+        // self-asignment check
+        if (this != &rhs) {
+            this->vardas = rhs.vardas;
+            this->pavarde = rhs.pavarde;
+            this->egz = rhs.egz;
+
+            this->paz = rhs.paz;
+            this->vid = rhs.vid;
+            this->med = rhs.med;
+        }
+
+        return *this;
+    }
+
+    /// Move konstruktorius
+    /// <param name="other"></param>
+    studentas(studentas&& other) {
+        vardas = other.vardas;
+        pavarde = other.pavarde;
+        paz = other.paz;
+        egz = other.egz;
+        vid = other.vid;
+        med = other.med;
+        other.vardas.clear();
+        other.pavarde.clear();
+        other.paz.clear();
+        other.egz = 0;
+        other.vid = 0;
+        other.med = 0;
+    }
+
+    /// Move paskyrimas.
+    /// <param name="other"></param>
+    /// <returns></returns>
+    studentas& operator=(studentas&& other) {
+        // self-asignment check
+        if (this != &other) {
+
+            if (!this->paz.empty()) paz.clear();
+            if (!this->vardas.empty()) vardas.clear();
+            if (!this->pavarde.empty()) pavarde.clear();
+
+            egz = other.egz;
+            vardas = other.vardas;
+            pavarde = other.pavarde;
+            paz = other.paz;
+            vid = other.vid;
+            med = other.med;
+            other.vardas.clear();
+            other.pavarde.clear();
+            other.paz.clear();
+            other.egz = 0;
+            other.vid = 0;
+            other.med = 0;
+        }
+
+        return *this;
+    }
+
+    /// Gražina egzamino pažymį.
+    /// <returns></returns> Egzaminas
+    inline int getegz() const { return egz; }
+    /// Nustato egzamino pažymį.
+    /// <param name="_value"></param> Egzamino reikšmė.
+    inline void setegz(int _value) { this->egz = _value; }
+
+
+    /// Gražina vidurkio pažymį.
+    /// <returns></returns> Vidurkis
+    inline float getvid() const { return vid; }
+    /// Nustato vidurkio pažymį.
+    /// <param name="_value"></param> Vidurkio reikšmė.
+    inline void setvid(float _value) { this->vid = _value; }
+
+    /// Gražina mediana.
+    /// <returns></returns> Mediana
+    inline float getmed() const { return med; }
+    /// Nustato Mediana.
+   /// <param name="_value"></param> Medianos reikšmė.
+    inline void setmed(float _value) { this->med = _value; }
+
+    /// Gražina vardą.
+    /// <returns></returns> Vardas
+    inline std::string getvardas() const { return vardas; }    // get'eriai, inline
+    /// Nustato vardą.
+   /// <param name="_value"></param> Vardo reikšmė.
+    inline void setvardas(string _value) { this->vardas = _value; }
+
+    /// Gražina pavardę.
+    /// <returns></returns> Pavarde
+    inline std::string getpavarde() const { return pavarde; }  // get'eriai, inline
+    /// Nustato pavardę.
+   /// <param name="_value"></param> Pavardės reikšmė.
+    inline void setpavarde(string _value) { this->pavarde = _value; }
+
+    /// Gražina ND pažymius.
+    /// <returns></returns> ND pažymiai
+    inline std::vector<int> getpaz() const { return paz; }  // get'eriai, inline
+    /// Nustato pažymius po viena reikšmę.
+    /// Tik po vieną skaičių pakeičia, įvedamas int kintamasis. Naudojama vector push_back.
+    /// <param name="pazymys"></param> ND pažymys.
+    inline void setpaz(int pazymys) { paz.push_back(pazymys); }
+    /// Nustato pažymius.
+    /// Nustato pagal visus pažymius, įvedamas vektorius.
+    /// <param name="v"></param> ND pažymiai.
+    inline void setpazdef(vector<int> v) { paz = v; }
+    /// Pažymių vektoriaus dydžio pakeitimas.
+    /// <param name="sz"></param> ND pažymių kiekis
+    inline void pazresize(int sz) { paz.resize(sz); }
+
+    /// Vidurkio funkcija.
+    /// Apskaičiuoja vidurkio pažymį pagal turimas nd ir egzamino pažymius.
+    /// <returns></returns> Vidurkis reikšmė
+    DLL_API float vidurk();
+    /// Medianos funkcija.
+    /// Apskaičiuoja medianos pažymį pagal turimas nd ir egzamino pažymius.
+    /// <returns></returns> Mediana reikšmė
+    DLL_API float median();
+
+    /// Operatoriaus cout perdengimas.
+    /// Operatorius leidžia iškart spausdinti klasės studentas narius. Nereikia po viena narį kviesti.
+    /// <param name="os"></param> Cout perdengimas.
+    /// <param name="dt"></param> Studentas objektas.
+    DLL_API friend std::ostream& operator<<(std::ostream& os, const studentas& dt);
+    /// Operatoriaus cout perdengimas.
+    /// Operatorius leidžia iškart užpildyt visus klasės studentas narius. Nereikia po viena narį kviesti.
+    /// <param name="os"></param> Cin perdengimas.
+    /// <param name="dt"></param> Studentas objektas.
+    DLL_API friend std::istream& operator>>(std::istream& is, studentas& dt);
+
+    /// Destruktorius
+    ~studentas() {vardas.clear(), pavarde.clear(), paz.clear(); egz = 0; vid = 0; med = 0; }
+};
+
+extern "C"
+/// Studentų objektų palyginimas.
+/// Palygina du studentus pagal vidurkį. Grąžina mažesenį.
+/// <param name=""></param> Studentas 1
+/// <param name=""></param> Studentas 2
+/// <returns></returns> Studentas 1 arba Studentas 2
+DLL_API bool compare(const studentas&, const studentas&); //pagal vidurkius
+
+extern "C"
+/// Atsitiktinai generuoja studentą.
+/// 
+/// Atsitktinio generavimo metodu generuojami studento atributai.
+/// <param name="nd_kiek"></param> ND pažymių kiekis
+/// <param name="i"></param> Indexas
+/// <param name="temp"></param> Studentas objektas
+/// <returns></returns> Sugeneruotas studentas objektas
+DLL_API void generate(int nd_kiek, int i, studentas& temp);
+
+extern "C"
+/// Atspausdina studento objekto vardą, pavardę ir vidurkį arba medianą.
+/// <param name="pas2"></param> Pasirinkimas tarp vidurkio ir medianos
+/// <param name="pas3"></param> Pasirinkimas tarp terminalo ir tekstinio failo spausdinimo
+/// <param name="out_f"></param> Rezultatų tekstinis failas
+/// <param name="temp"></param> Studento objektas
+/// <returns></returns>
+DLL_API void spausd(char pas2, char pas3, std::ofstream& out_f, studentas temp);
+
+extern "C"
+/// Perskaito tekstinį failą ir pagal duomenis sukuria studentų objektų vektorių.
+/// <param name="open_f"></param> Skaitomas tekstinis failas
+/// <param name="grupe"></param> Studentų objektų vektorius
+DLL_API void read(std::ifstream& open_f, vector<studentas>& grupe);
+
+extern "C"
+/// Atsitiktinai sugeneruoja ir atspausdina tekstiniame faile studentų vardus, pavardes, ND ir egzamino pažymius
+/// <param name="nd_kiekis"></param> ND pažymių kiekis
+/// <param name="i"></param> Indexas
+/// <param name="out_f"></param> Rezultatų tekstinis failas
+/// <returns></returns>
+DLL_API void failai(int nd_kiekis, int i, std::ofstream& out_f);
+
+extern "C"
+/// Išrinkimas tarp Nelaimėlių ir Kietekų.
+/// 
+/// Išrenka pagal vidurkį studentus ir paskirsto į dvi naujas grupes Nelaimėliai ir Kietekai.
+/// <param name="grupe"></param> Visų studentų vektorius
+/// <param name="Nel"></param> Nelaimėlių vektorius (Kurių vidurkis<5.00)
+/// <param name="Kiet"></param> Kietekų vektorius (Kurių vidurkis>=5.00)
+DLL_API void isrinkimas1(vector<studentas> grupe, vector<studentas>& Nel, vector<studentas>& Kiet);
+
+extern "C"
+/// Antras išrinkimo būdas tarp Nelaimėlių ir Kietekų.
+/// 
+/// Išrenka pagal vidurkį studentus ir paskirsto į naują grupę Nelaimėliai ir iš senos grupės juos pašalina, taip lieka senoj grupė tik Kietekai.
+/// <param name="grupe"></param> Visų studentų vektorius
+/// <param name="Nel"></param> Nelaimėlių vektorius (Kurių vidurkis<5.00)
+DLL_API void isrinkimas2(vector<studentas>& grupe, vector<studentas>& Nel);
+
+
+#endif
---
+++ Source/dllmain.cpp
@@ -0,0 +1,190 @@
+﻿// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+# include "DLL.h"
+
+void generate(int nd_kiekis, int i, studentas& temp) { // Atsitiktinio generavimo funkcija
+
+	using hrClock = std::chrono::high_resolution_clock;
+	std::mt19937_64 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
+	temp.setvardas(("Vardas" + std::to_string(i + 1)));
+	temp.setpavarde(("Pavarde" + std::to_string(i + 1)));
+	std::uniform_int_distribution<int>dist2(0, 10); // Pažymiams
+	for (int j = 0; j < nd_kiekis; j++)
+	{
+		temp.setpaz((dist2(mt)));
+	}// Generuojami pažymiai
+	temp.setegz((dist2(mt))); // Generuojami egzamino pažymys
+	temp.setvid(temp.vidurk());
+	temp.setmed(temp.median());
+}
+
+void read(std::ifstream& open_f, vector<studentas>& grupe) {
+
+	std::vector<std::string> splited;
+	std::string word, line;
+	int max = 0;
+	double sk = 0.0; // Iš viso žodžių faile
+	int stul = 0; //stulpelių sk.
+	studentas temp;
+	auto start2 = std::chrono::high_resolution_clock::now(); auto st2 = start2;
+	if (!open_f.fail()) {
+
+		getline(open_f, line);
+		splited.push_back(line);
+		while (getline(open_f, line)) {
+			++max;
+			int a;
+			string v, p = "";
+			std::stringstream ss(line);
+			ss >> v >> p;
+			temp.setvardas(v); temp.setpavarde(p);
+			while (ss >> a) {
+				temp.setpaz(a);
+			}
+			temp.setegz(temp.getpaz().back());
+			temp.getpaz().pop_back();
+			temp.setvid(temp.vidurk());
+			temp.setmed(temp.median());
+			grupe.push_back(temp);
+			temp.pazresize(0);
+		}
+	}
+	open_f.close();
+	splited.resize(0);
+
+	auto end2 = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff2 = end2 - start2; // Skirtumas (s)
+	std::cout << "Failo is " << max << " iraso nuskaitymo laikas: " << diff2.count() << " s\n";
+}
+
+void spausd(char pas2, char pas3, std::ofstream& out_f, studentas temp) // Rezultatų atspausdinimas
+{
+	if (pas3 == 'n') { // Jei norima spausdinti į terminalą
+		cout << left << setw(20) << temp.getvardas();
+		cout << left << setw(25) << temp.getpavarde();
+		if (pas2 == '1') { cout << left << setw(30) << std::fixed << setprecision(2) << temp.getvid() << endl; }
+		else if (pas2 == '2') { cout << left << setw(30) << std::fixed << setprecision(2) << temp.getmed() << endl; }
+		else if (pas2 == '3') { cout << left << setw(30) << std::fixed << setprecision(2) << temp.getvid() << setw(35) << std::fixed << setprecision(2) << temp.getmed() << endl; }
+	}
+	else if (pas3 == 'y') { //Jei norima spausdinti į failą
+		out_f << setw(20) << left << temp.getvardas();
+		out_f << setw(25) << left << temp.getpavarde();
+		if (pas2 == '1') { out_f << std::fixed << setprecision(2) << setw(30) << left << temp.getvid() << '\n'; }
+		else if (pas2 == '2') { out_f << setw(30) << std::fixed << setprecision(2) << left << temp.getmed() << endl; }
+		else if (pas2 == '3') { out_f << setw(30) << std::fixed << setprecision(2) << left << temp.getvid() << setw(30) << std::fixed << setprecision(2) << left << temp.getmed() << endl; }
+	}
+}
+
+float studentas::vidurk() // Vidurkio skaičiavimas
+{
+	float nam_vid = 0, sum = 0;
+	for (const auto& i : getpaz()) { sum = sum + i; }
+	if (getpaz().size() == 0) pazresize(1);
+	nam_vid = sum / getpaz().size();
+	float vid = float(nam_vid * 0.4 + getegz() * 0.6);
+	return vid;
+}
+
+float studentas::median() //Medianos skaičiavimas
+{
+	float med;
+	vector<int> vectkopija;
+	typedef vector<int>::size_type vec_sz;
+	vectkopija = getpaz();
+	//vectkopija.push_back(getegz());
+	vec_sz size = vectkopija.size();
+	sort(vectkopija.begin(), vectkopija.end());
+	vec_sz mid = size / 2;
+	med= size % 2 == 0 ? float(vectkopija[mid] + vectkopija[mid - 1]) / 2 : vectkopija[mid];
+	return med * 0.4 + egz * 0.6;
+}
+
+void failai(int nd_kiekis, int i, std::ofstream& out_f) { // Atspausdinimi studentų failai be rezultatų
+
+	using hrClock = std::chrono::high_resolution_clock;
+	std::mt19937_64 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
+	std::uniform_int_distribution<int>dist(0, 10); // Pažymiams
+	out_f << setw(20) << left << "Vardas" + std::to_string(i + 1);
+	out_f << setw(25) << left << "Pavarde" + std::to_string(i + 1);
+	for (int i = 0; i < nd_kiekis; i++) {
+		out_f << setw(5) << dist(mt) << setw(5);
+	}
+	out_f << dist(mt) << "\n";
+}
+
+void isrinkimas1(vector<studentas> grupe, vector<studentas>& Nel, vector<studentas>& Kiet) {
+
+	auto starts1 = std::chrono::high_resolution_clock::now(); auto sts1 = starts1; //Pradedamas dalijimas į dvi grupes
+	size_t laikui = grupe.size();
+
+	Nel.resize(grupe.size());
+	Kiet.resize(grupe.size());
+	auto it = std::copy_if(grupe.begin(), grupe.end(), Nel.begin(), [](const auto& s) {return (s.getvid() < 5.00); });
+	Nel.resize(std::distance(Nel.begin(), it));
+	auto it2 = std::copy_if(grupe.begin(), grupe.end(), Kiet.begin(), [](const auto& s) {return (s.getvid() >= 5.00); });
+	Kiet.resize(std::distance(Kiet.begin(), it2));
+
+	auto ends1 = std::chrono::high_resolution_clock::now();						// Baigiamas dalijimas į dvi grupes
+	std::chrono::duration<double> diffs1 = ends1 - starts1;						// Skirtumas (s)
+	std::cout << laikui << " iraso dalijamo i dvi grupes laikas: " << diffs1.count() << " s\n";
+}
+
+void isrinkimas2(vector<studentas>& grupe, vector<studentas>& Nel) {
+
+	auto starts2 = std::chrono::high_resolution_clock::now(); auto sts2 = starts2; //Pradedamas dalijimas į dvi grupes
+	size_t laikui = grupe.size();
+
+	Nel.resize(grupe.size());
+	auto it = std::copy_if(grupe.begin(), grupe.end(), Nel.begin(), [](const auto& s) {return (s.getvid() < 5.00); });
+	Nel.resize(std::distance(Nel.begin(), it));
+	auto endit = remove_if(grupe.begin(), grupe.end(), [](const auto& s) {return (s.getvid() < 5.00); });
+	grupe.erase(endit, grupe.end());
+
+	auto ends2 = std::chrono::high_resolution_clock::now();						// Baigiamas dalijimas į dvi grupes
+	std::chrono::duration<double> diffs2 = ends2 - starts2;						// Skirtumas (s)
+	std::cout << laikui << " iraso dalijamo i dvi grupes laikas: " << diffs2.count() << " s\n";
+}
+
+bool compare(const studentas& a, const studentas& b) { return a.getvid() < b.getvid(); }
+
+std::ostream& operator<<(std::ostream& os, const studentas& dt) {
+	os << dt.vardas << " " << dt.pavarde << " ";
+	for (auto& i : dt.paz) os << i << " ";
+	os << dt.egz << " " << dt.vid << " " << dt.med << " ";
+	return os;
+}
+
+std::istream& operator>>(std::istream& is, studentas& dt) {
+
+	int nd_kiekis = 0;
+	int p;
+	cout << "Iveskite varda ir pavarde: "; cin >> dt.vardas >> dt.pavarde;
+
+	cout << "Iveskite nd pazymiu kieki:"; cin >> nd_kiekis;
+	dt.paz.resize(nd_kiekis);
+
+	for (int i = 0; i < nd_kiekis; i++) {
+		cin >> p;
+
+		dt.paz.push_back(p); // Vedami nd pažymiai
+	};
+
+	do {
+		cout << "Iveskite egzamino paz. (Turi buti skaitmuo)" << endl;
+		if (cin >> dt.egz) break;
+		cin.clear();
+		//std::cin.ignore(numeric_limits<std::streamsize>::max(), '\n');
+	} while (true);
+
+	dt.vid = dt.vidurk();
+	dt.med = dt.median();
+
+	cout << "Duomenys irasyti" << endl;
+
+	return is;
+}
+
+zmogus::~zmogus() // Explicit destructor call
+{
+	vardas.clear(); pavarde.clear();
+}
